df102f8c95c8636f3fc368d49366a1140e427a97
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-12 17:44:10 +0100

    connect to mongodb on server startup

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from dotenv import dotenv_values
+from pymongo import MongoClient
+
+config = dotenv_values(".env")
 
 app = FastAPI()
 
+
+@app.on_event("startup")
+def startup_db_client():
+    app.mongodb_client = MongoClient(config["MONGO_URI"])
+    app.database = app.mongodb_client[config["DB_NAME"]]
+    print("Connected to the MongoDB!")
 
 @app.get("/")
 async def root():
