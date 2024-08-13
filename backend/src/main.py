ef30e47e4f4bc46c4dcca7fa03d6cd8d2b6fb2fb
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-13 19:03:35 +0100

    fixed import

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-import router as menu_router
+from router import router as menu_router
 
 app = FastAPI(
     title="FARM Stack Menu API", 
     summary="Backend for fullstack menu application using FastApi, React and Mongodb"
     )
 
-app.include_router(menu_router, prefix="/api/v1", tags=["menu"])
+app.include_router(router=menu_router, prefix="/api/v1", tags=["menu"])
