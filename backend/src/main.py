9539774c935dcf68e9817c67d528c061a9a9ca25
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-13 18:51:11 +0100

    refactored code

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,28 +1,9 @@
 from fastapi import FastAPI
-from dotenv import dotenv_values
-from pymongo import MongoClient
-from contextlib import asynccontextmanager
+import router as menu_router
 
-config = dotenv_values(".env")
+app = FastAPI(
+    title="FARM Stack Menu API", 
+    summary="Backend for fullstack menu application using FastApi, React and Mongodb"
+    )
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    ## connect to database on server startup
-    app.mongodb_client = MongoClient(config["MONGO_URI"])
-    app.database = app.mongodb_client[config["DB_NAME"]]
-    print("Connected to the MongoDB!")
-
-    yield
-
-    ## disconnect mongodb connection
-    app.mongodb_client.close()
-    print("Connection to the MongoDB closed!")
-
-
-app = FastAPI(lifespan=lifespan)
-
-
-## root 
-@app.get("/")
-async def root():
-    return {"message": "Welcome!"}
+app.include_router(menu_router, prefix="/api/v1", tags=["menu"])
