29f1deb59e7cc13300dbb130dc7d9a5a603b1870
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-13 18:29:29 +0100

    added route to get menu by Id

--- backend/src/router.py
+++ backend/src/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, status, Depends, HTTPException
 from database import menu_collection
 from models import MenuModel, MenuList
+from bson import ObjectId
 
 router = APIRouter()
 
@@ -14,8 +15,23 @@ router = APIRouter()
 async def add_menu_item():
     pass
 
-async def get_single_menu_item():
-    pass
+@router.get(
+    "/menu/{id}",
+    response_description="Get menu by id",
+    response_model=MenuModel,
+    status_code=status.HTTP_200_OK,
+    response_model_by_alias=False,
+)
+async def get_single_menu_item(id: str):
+    """
+    Get single menu by `id`.
+    """
+    menu_exists = await menu_collection.find_one({"_id": ObjectId(id)})
+    if menu_exists is not None:
+        return menu_exists
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="menu does not exist")
+
 
 @router.get(
     "/menus/",
@@ -31,6 +47,7 @@ async def get_all_menu_items():
     The response is unpaginated and limited to 100 results.
     """
     return MenuList(menus= await menu_collection.find().to_list(100))
+
 
 async def delete_menu_item():
     pass
