474c73fe0eae49a1975810ce78c176b59b8f4395
alahirajeffrey <alahirajeffrey@gmail.com>
2024-08-15 18:07:02 +0100

    code refcator

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,10 +7,12 @@ const Navbar = () => {
 
   return (
     <div className="grid grid-cols-3 gap-4 bg-blue-800 py-3 ">
-      <div className="col-span-2 px-5 font-black text-3xl ">
-        Sholly Restaurant Menu
+      <div className="col-span-2 flex items-center justify-center">
+        <h1 className=" px-5 font-black text-3xl ">Sholly Restaurant Menu</h1>
       </div>
-      <Button title={"Add Menu"} onClick={addMenuItem} />
+      <div className="flex justify-center items-center">
+        <Button title={"Add Menu"} onClick={addMenuItem} />
+      </div>
     </div>
   );
 };
